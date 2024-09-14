97b51846679ddbe1bac1a4c16600f1682a36fe27
MarinaErokhova <erokhovamn@gmail.com>
2024-09-14 15:57:30 +0300

    Add files via upload

---
+++ module_1_4_string methods.py
@@ -0,0 +1,8 @@
+my_string = input()
+#number_of_letters
+print(my_string.__len__())
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
